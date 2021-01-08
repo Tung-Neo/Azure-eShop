98ab983b2278f2d3b20de25b948bdd15c5119580
Tung-Neo <62163195+Tung-Neo@users.noreply.github.com>
2021-01-08 23:10:27 +0700

    eShop2

--- MovieApp/Controllers/HomeController.cs
+++ MovieApp/Controllers/HomeController.cs
@@ -11,27 +11,21 @@ namespace MovieApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private IAppRepositiry repositiry;
+        public int PageSize = 4;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IAppRepositiry repo)
         {
-            _logger = logger;
+            repositiry = repo;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+        //public IActionResult Index() => View(repositiry.Products);
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        public ViewResult Index(int productPage = 1)
+            => View(repositiry.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize)
+                );
     }
 }
